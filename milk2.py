c48aa72f13214565a36ccfafb2bb841b373846db
github-is-stupid <59267390+github-is-stupid@users.noreply.github.com>
2020-11-01 12:37:53 -0800

    start

---
+++ milk2.py
@@ -0,0 +1,18 @@
+"""
+ID: benhua41
+LANG: PYTHON3
+TASK: milk2
+"""
+
+fin = open('milk2.in', 'r')
+fout = open('milk2.out', 'w')
+print(fin.read())
+
+lines = []
+for line in fin:
+    print(line)
+    lines.append(line)
+
+final = str() + "\n"
+fout.write(final)
+fout.close()
